37d35b1341b4234729f252c5610b14a046e8a59d
Matt DeMartino <matt.s.demartino@gmail.com>
2024-02-14 16:03:58 -0500

    Create subtract test

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -5,10 +5,19 @@ import (
 	"testing"
 )
 
-func TestADd(t *testing.T) {
+func TestAdd(t *testing.T) {
 	t.Parallel()
 	var want float64 = 4
 	got := calculator.Add(2, 2)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+	var want float64 = 2
+	got := calculator.Subtract(4, 2)
 	if want != got {
 		t.Errorf("want %f, got %f", want, got)
 	}
