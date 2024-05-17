b64611a6dae9fc6009f8d9c30b75ea8ac7e91024
Matt DeMartino <matt.s.demartino@gmail.com>
2024-05-17 13:52:59 -0400

    Create passing Area methods

--- learn-go-with-tests/perimeter/perimeter.go
+++ learn-go-with-tests/perimeter/perimeter.go
@@ -1,26 +1,28 @@
 package perimeter
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
 func (r Rectangle) Area() float64 {
-	return 0
-}
-
-func (c Circle) Area() float64 {
-	return 0
+	return r.Height * r.Width
 }
 
 type Circle struct {
 	Radius float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
-// func Area(rectangle Rectangle) float64 {
-// 	return rectangle.Width * rectangle.Height
-// }
+func Perimeter(rectangle Rectangle) float64 {
+	return (rectangle.Width + rectangle.Height) * 2
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
--- learn-go-with-tests/perimeter/perimeter_test.go
+++ learn-go-with-tests/perimeter/perimeter_test.go
@@ -6,27 +6,25 @@ func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
 	got := Perimeter(rectangle)
 	want := 40.0
-
-	if got != want {
+	if want != got {
 		t.Errorf("got %.2f, want %.2f", got, want)
 	}
 }
 
 func TestArea(t *testing.T) {
 	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
+		rectangle := Rectangle{3.0, 5.0}
 		got := rectangle.Area()
-		want := 72.0
-
-		if got != want {
-			t.Errorf("got %g, want %g", got, want)
+		want := 15.0
+		if want != got {
+			t.Errorf("got %.g, want %.g", want, got)
 		}
 	})
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
 		got := circle.Area()
 		want := 314.1592653589793
-		if want != got {
+		if got != want {
 			t.Errorf("got %g, want %g", got, want)
 		}
 	})
