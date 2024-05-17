1534e8926f7522decd974877784db422bce390e3
Matt DeMartino <matt.s.demartino@gmail.com>
2024-05-17 13:51:36 -0400

    Create Area methods on structs

--- learn-go-with-tests/perimeter/perimeter.go
+++ learn-go-with-tests/perimeter/perimeter.go
@@ -5,10 +5,22 @@ type Rectangle struct {
 	Height float64
 }
 
+func (r Rectangle) Area() float64 {
+	return 0
+}
+
+func (c Circle) Area() float64 {
+	return 0
+}
+
+type Circle struct {
+	Radius float64
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
-}
+// func Area(rectangle Rectangle) float64 {
+// 	return rectangle.Width * rectangle.Height
+// }
