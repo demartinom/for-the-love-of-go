5847f8ddf2eb98201efe4b197dca344d59171f19
Matt DeMartino <matt.s.demartino@gmail.com>
2024-05-17 11:02:35 -0400

    Add french and add spanish consts

--- learn-go-with-tests/hello/hello.go
+++ learn-go-with-tests/hello/hello.go
@@ -8,17 +8,27 @@ import "fmt"
 // 	fmt.Println("Hello, world")
 // }
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const french = "French"
+const frenchHelloPrefix = "Bonjour, "
 
 // Says hello
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+	if language == french {
+		return frenchHelloPrefix + name
 	}
 	return englishHelloPrefix + name // Say hello
 }
 
 // Main function
 func main() {
-	fmt.Println(Hello("world")) // A thing
+	fmt.Println(Hello("world", "")) // A thing
 }
