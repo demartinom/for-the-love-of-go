bea23f58431b3f027754c9839ea0b5584690568c
Matt DeMartino <matt.s.demartino@gmail.com>
2024-05-17 13:35:13 -0400

    Create Area test and function

--- learn-go-with-tests/perimeter/perimeter.go
+++ learn-go-with-tests/perimeter/perimeter.go
@@ -1,5 +1,9 @@
 package perimeter
 
-func Perimeter(width float64, height float64) float64 {
+func Perimeter(width, height float64) float64 {
 	return 2 * (width + height)
+}
+
+func Area(width, height float64) float64 {
+	return width * height
 }
