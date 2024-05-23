51ea0d56db27d2992521c749be5a13c1a2882c52
Matt DeMartino <matt.s.demartino@gmail.com>
2024-05-23 14:16:28 -0400

    Add triangle struct

--- learn-go-with-tests/perimeter/perimeter_test.go
+++ learn-go-with-tests/perimeter/perimeter_test.go
@@ -11,22 +11,54 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
+// func TestArea(t *testing.T) {
+// 	checkArea := func(t testing.TB, shape Shape, want float64) {
+// 		t.Helper()
+// 		got := shape.Area()
+// 		if got != want {
+// 			t.Errorf("got %g, want %g", got, want)
+// 		}
+// 	}
+// 	t.Run("rectangles", func(t *testing.T) {
+// 		rectangle := Rectangle{12, 6}
+// 		got := rectangle.Area()
+// 		checkArea(t, rectangle, got)
+// 	})
+// 	t.Run("circles", func(t *testing.T) {
+// 		circle := Circle{10}
+// 		got := circle.Area()
+// 		checkArea(t, circle, got)
+// 	})
+// }
+
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g, want %g", got, want)
-		}
+	testCases := []struct {
+		desc  string
+		shape Shape
+		want  float64
+	}{
+		{
+			"rectangles",
+			Rectangle{12, 6},
+			72.0,
+		},
+		{
+			"circles",
+			Circle{10},
+			314.1592653589793,
+		},
+		{
+			"triangles",
+			Triangle{12, 6},
+			36.0,
+		},
 	}
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		got := rectangle.Area()
-		checkArea(t, rectangle, got)
-	})
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		got := circle.Area()
-		checkArea(t, circle, got)
-	})
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			got := tC.shape.Area()
+			if got != tC.want {
+				t.Errorf("got %g, want %g", got, tC.want)
+			}
+		})
+	}
 }
