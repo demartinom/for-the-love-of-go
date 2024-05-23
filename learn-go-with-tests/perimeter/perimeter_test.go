b2a912cbe736bb8facc48fcdf6acab8257a0c4eb
Matt DeMartino <matt.s.demartino@gmail.com>
2024-05-23 14:16:50 -0400

    Remove old test

--- learn-go-with-tests/perimeter/perimeter_test.go
+++ learn-go-with-tests/perimeter/perimeter_test.go
@@ -11,26 +11,6 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
-// func TestArea(t *testing.T) {
-// 	checkArea := func(t testing.TB, shape Shape, want float64) {
-// 		t.Helper()
-// 		got := shape.Area()
-// 		if got != want {
-// 			t.Errorf("got %g, want %g", got, want)
-// 		}
-// 	}
-// 	t.Run("rectangles", func(t *testing.T) {
-// 		rectangle := Rectangle{12, 6}
-// 		got := rectangle.Area()
-// 		checkArea(t, rectangle, got)
-// 	})
-// 	t.Run("circles", func(t *testing.T) {
-// 		circle := Circle{10}
-// 		got := circle.Area()
-// 		checkArea(t, circle, got)
-// 	})
-// }
-
 func TestArea(t *testing.T) {
 	testCases := []struct {
 		desc  string
@@ -38,19 +18,19 @@ func TestArea(t *testing.T) {
 		want  float64
 	}{
 		{
-			"rectangles",
-			Rectangle{12, 6},
-			72.0,
+			desc:  "rectangles",
+			shape: Rectangle{12, 6},
+			want:  72.0,
 		},
 		{
-			"circles",
-			Circle{10},
-			314.1592653589793,
+			desc:  "circles",
+			shape: Circle{10},
+			want:  314.1592653589793,
 		},
 		{
-			"triangles",
-			Triangle{12, 6},
-			36.0,
+			desc:  "triangles",
+			shape: Triangle{12, 6},
+			want:  36.0,
 		},
 	}
 	for _, tC := range testCases {
