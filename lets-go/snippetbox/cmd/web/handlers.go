dea236102de7df8f8a4097173fdadd9f9f5e5cb6
Matt DeMartino <matt.s.demartino@gmail.com>
2024-06-20 15:27:06 -0400

    Use template in html parser

--- lets-go/snippetbox/cmd/web/handlers.go
+++ lets-go/snippetbox/cmd/web/handlers.go
@@ -13,13 +13,22 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl.html")
+
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+
+	//Reads htmls files
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Service Error", 500)
 		return
 	}
-	err = ts.Execute(w, nil)
+
+	//Writes the base template to the response body
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
