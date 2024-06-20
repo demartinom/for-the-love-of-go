ff198dcfd62f952cc448eb597306a1817605bbba
Matt DeMartino <matt.s.demartino@gmail.com>
2024-06-20 15:51:06 -0400

    Create handler for files

--- lets-go/snippetbox/cmd/web/main.go
+++ lets-go/snippetbox/cmd/web/main.go
@@ -7,9 +7,16 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	//Create file server
+	//Relative to project directory
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
+
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
