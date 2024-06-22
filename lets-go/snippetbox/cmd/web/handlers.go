56d7912a3fe16f1ebbccd61cb7c5b389803d2b82
Matt DeMartino <matt.s.demartino@gmail.com>
2024-06-22 16:18:38 -0400

    Use error helpers

--- lets-go/snippetbox/cmd/web/handlers.go
+++ lets-go/snippetbox/cmd/web/handlers.go
@@ -22,23 +22,20 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	//Reads htmls files
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Service Error", 500)
-		return
+		app.serverError(w, err)
 	}
 
 	//Writes the base template to the response body
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err)
 	}
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
@@ -47,7 +44,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("Create a new snippet..."))
