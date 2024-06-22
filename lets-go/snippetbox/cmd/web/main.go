c15ced4d3e79f49cd6801a2494e50c7b58a70627
Matt DeMartino <matt.s.demartino@gmail.com>
2024-06-22 15:42:36 -0400

    Create http.Server struct

--- lets-go/snippetbox/cmd/web/main.go
+++ lets-go/snippetbox/cmd/web/main.go
@@ -28,7 +28,13 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
 	infoLog.Printf("Staring at server %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
