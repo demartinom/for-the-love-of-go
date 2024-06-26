1e0cd6ec67e0aa46e8cd877fedec5dab8f25a55e
Matt DeMartino <matt.s.demartino@gmail.com>
2024-06-26 14:06:19 -0400

    Create function to open DB

--- lets-go/snippetbox/cmd/web/main.go
+++ lets-go/snippetbox/cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -15,12 +18,21 @@ type application struct {
 func main() {
 	//CLI flag for setting the port
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime-true", "MySQL data source name")
+
 	//Parse the flag
 	flag.Parse()
 
 	//Create loggers for writing messages
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -34,6 +46,17 @@ func main() {
 	}
 
 	infoLog.Printf("Staring at server %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
