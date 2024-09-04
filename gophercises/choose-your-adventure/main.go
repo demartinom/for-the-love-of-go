4ce867344afb1ca0c656b1cdb7d0ea97aa50d2ab
Matt DeMartino <matt.s.demartino@gmail.com>
2024-09-04 15:54:27 -0400

    Create json types

--- gophercises/choose-your-adventure/main.go
+++ gophercises/choose-your-adventure/main.go
@@ -4,10 +4,36 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
 )
+
+type Arc struct {
+	Intro     Title `json:"intro"`
+	NewYork   Title `json:"new-york"`
+	Debate    Title `json:"debate"`
+	SeanKelly Title `json:"sean-kelly"`
+	MarkBates Title `json:"mark-bates"`
+	Denver    Title `json:"denver"`
+	Home      Title `json:"home"`
+}
+
+type Title struct {
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []Option `json:"options"`
+}
+
+type Option struct {
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	files := []string{"./web/html/base.tmpl.html"}
+
+	var story Story
+	err := json.Unmarshal([]byte(),%story)
+
 	handleTemplates(w, files)
 }
 
