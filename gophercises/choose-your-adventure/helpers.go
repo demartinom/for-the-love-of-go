590b576b6754ead165d13d28ec321e4a33137e98
Matt DeMartino <matt.s.demartino@gmail.com>
2024-09-04 16:06:52 -0400

    Create json reader helper function

--- gophercises/choose-your-adventure/helpers.go
+++ gophercises/choose-your-adventure/helpers.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 )
 
 func handleTemplates(w http.ResponseWriter, templates []string) {
@@ -17,4 +20,18 @@ func handleTemplates(w http.ResponseWriter, templates []string) {
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+func readJson(file string) []byte {
+	jsonFile, err := os.Open("gopher.json")
+	if err != nil {
+		log.Fatalf("failed to open file: %s", err)
+	}
+	defer jsonFile.Close()
+
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		log.Fatalf("failed to read file: %s", err)
+	}
+	return byteValue
 }
