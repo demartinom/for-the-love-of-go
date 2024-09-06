0c533a6afe7aef752137e1f5cf1e66c4b5b7f9a1
Matt DeMartino <matt.s.demartino@gmail.com>
2024-09-06 12:47:29 -0400

    Use single page template for all story pages

--- gophercises/choose-your-adventure/routes.go
+++ gophercises/choose-your-adventure/routes.go
@@ -4,16 +4,16 @@ import "net/http"
 
 func home(w http.ResponseWriter, r *http.Request) {
 
-	files := []string{"./web/html/base.tmpl.html", "./web/html/home.tmpl.html"}
+	files := []string{"./web/html/base.tmpl.html", "./web/html/page.tmpl.html"}
 	handleTemplates(w, files, storyText.Intro)
 }
 
 func newYork(w http.ResponseWriter, r *http.Request) {
-	files := []string{"./web/html/base.tmpl.html", "./web/html/newyork.tmpl.html"}
+	files := []string{"./web/html/base.tmpl.html", "./web/html/page.tmpl.html"}
 	handleTemplates(w, files, storyText.NewYork)
 }
 
 func denver(w http.ResponseWriter, r *http.Request) {
-	files := []string{"./web/html/base.tmpl.html", "./web/html/denver.tmpl.html"}
+	files := []string{"./web/html/base.tmpl.html", "./web/html/page.tmpl.html"}
 	handleTemplates(w, files, storyText.Denver)
 }
