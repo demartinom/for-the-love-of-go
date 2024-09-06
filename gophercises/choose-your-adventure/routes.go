2970b8f156f6b33136b775ea256fb8b18662a232
Matt DeMartino <matt.s.demartino@gmail.com>
2024-09-06 13:56:33 -0400

    Put files in global var

--- gophercises/choose-your-adventure/routes.go
+++ gophercises/choose-your-adventure/routes.go
@@ -2,37 +2,31 @@ package main
 
 import "net/http"
 
-func intro(w http.ResponseWriter, r *http.Request) {
+var files = []string{"./web/html/base.tmpl.html", "./web/html/page.tmpl.html"}
 
-	files := []string{"./web/html/base.tmpl.html", "./web/html/page.tmpl.html"}
+func intro(w http.ResponseWriter, r *http.Request) {
 	handleTemplates(w, files, storyText.Intro)
 }
 
 func newYork(w http.ResponseWriter, r *http.Request) {
-	files := []string{"./web/html/base.tmpl.html", "./web/html/page.tmpl.html"}
 	handleTemplates(w, files, storyText.NewYork)
 }
 
 func debate(w http.ResponseWriter, r *http.Request) {
-	files := []string{"./web/html/base.tmpl.html", "./web/html/page.tmpl.html"}
 	handleTemplates(w, files, storyText.Debate)
 }
 
 func seanKelly(w http.ResponseWriter, r *http.Request) {
-	files := []string{"./web/html/base.tmpl.html", "./web/html/page.tmpl.html"}
 	handleTemplates(w, files, storyText.SeanKelly)
 }
 
 func markBates(w http.ResponseWriter, r *http.Request) {
-	files := []string{"./web/html/base.tmpl.html", "./web/html/page.tmpl.html"}
 	handleTemplates(w, files, storyText.MarkBates)
 }
 
 func denver(w http.ResponseWriter, r *http.Request) {
-	files := []string{"./web/html/base.tmpl.html", "./web/html/page.tmpl.html"}
 	handleTemplates(w, files, storyText.Denver)
 }
 func home(w http.ResponseWriter, r *http.Request) {
-	files := []string{"./web/html/base.tmpl.html", "./web/html/page.tmpl.html"}
 	handleTemplates(w, files, storyText.Home)
 }
