16a255454889fc11238648cb195da20038c3725c
Matt DeMartino <matt.s.demartino@gmail.com>
2024-09-13 14:51:09 -0400

    Rewrite to take simple html rather than whole website

--- gophercises/link-parser/main.go
+++ gophercises/link-parser/main.go
@@ -1,49 +1,49 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
-	"net/http"
+	"strings"
 
 	"golang.org/x/net/html"
 )
 
 func main() {
-	siteFlag := flag.String("site", "https://gophercises.com/", "What site you want to parse")
-	flag.Parse()
-	fmt.Println("reading from site ", *siteFlag)
-
-	resp, err := http.Get(*siteFlag)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	doc, err := html.Parse(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+	//Test HTML
+	exHtml := `<html>
+	<body>
+	<h1>Hello!</h1>
+	<a href="/other-page">A link to another page</a>
+	</body>
+	</html>`
 	// Function taken from html docs
-	// Allows function to be referenced within itself
-	var f func(*html.Node)
 
-	f = func(n *html.Node) {
-		// Checks if node is an anchor tag
+	doc, err := html.Parse(strings.NewReader(exHtml))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var reading func(*html.Node)
+	reading = func(n *html.Node) {
+
 		if n.Type == html.ElementNode && n.Data == "a" {
-			// If it is an anchor tag, it looks over its attributes
 			for _, a := range n.Attr {
-				// If an href attribute is found, it prints the value of the href
 				if a.Key == "href" {
 					fmt.Println(a.Val)
 				}
+			}
 
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				if c.Type == html.TextNode {
+					fmt.Println(c.Data)
+				}
 			}
 		}
-		//Allows func to visit all children and siblings of current node
+
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
+			reading(c)
 		}
+
 	}
-	f(doc)
+	reading(doc)
 }
