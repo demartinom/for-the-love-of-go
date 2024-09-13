93485d703b8ddf55461705452814872dd6e6dc94
Matt DeMartino <matt.s.demartino@gmail.com>
2024-09-13 12:32:33 -0400

    Get html from site

--- gophercises/link-parser/main.go
+++ gophercises/link-parser/main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
 	siteFlag := flag.String("site", "https://gophercises.com/", "What site you want to parse")
 	flag.Parse()
 	fmt.Println("reading from site ", *siteFlag)
+
+	resp, err := http.Get(*siteFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
 }
