5fb8891d5ada6f0c620c863f6b4c180127a9a316
Matt DeMartino <matt.s.demartino@gmail.com>
2024-09-13 15:08:03 -0400

    Implement solution that returns slice of Link structs

--- gophercises/link-parser/main.go
+++ gophercises/link-parser/main.go
@@ -8,42 +8,64 @@ import (
 	"golang.org/x/net/html"
 )
 
+// struct for saving links
+type Link struct {
+	Href string
+	Text string
+}
+
 func main() {
-	//Test HTML
+	// Test HTML
 	exHtml := `<html>
 	<body>
 	<h1>Hello!</h1>
 	<a href="/other-page">A link to another page</a>
 	</body>
 	</html>`
-	// Function taken from html docs
 
+	// Create slice of Links
+	links := []Link{}
+
+	// Parse html
 	doc, err := html.Parse(strings.NewReader(exHtml))
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Init function for recursion
 	var reading func(*html.Node)
-	reading = func(n *html.Node) {
 
+	// Traverse html tree
+	reading = func(n *html.Node) {
+		// Look for a tags
 		if n.Type == html.ElementNode && n.Data == "a" {
+			// Init Link struct to store result
+			var link Link
+			// Find link in a tag and save to sstrcut
 			for _, a := range n.Attr {
 				if a.Key == "href" {
-					fmt.Println(a.Val)
+					link.Href = a.Val
 				}
 			}
 
+			// Find text in a tag and save to struct
 			for c := n.FirstChild; c != nil; c = c.NextSibling {
 				if c.Type == html.TextNode {
-					fmt.Println(c.Data)
+					link.Text = c.Data
 				}
 			}
+			// Add struct to slice of Link structs
+			links = append(links, link)
 		}
-
+		// Recursion
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
 			reading(c)
 		}
 
 	}
 	reading(doc)
+	// Print out Link structs in slice
+	for _, link := range links {
+		fmt.Printf("%+v\n", link)
+	}
 }
