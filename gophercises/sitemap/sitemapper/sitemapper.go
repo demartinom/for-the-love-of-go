86222f232886906c252e9b2c1a173c803a4f708e
Matt DeMartino <matt.s.demartino@gmail.com>
2024-09-17 16:04:45 -0400

    Read body of response

--- gophercises/sitemap/sitemapper/sitemapper.go
+++ gophercises/sitemap/sitemapper/sitemapper.go
@@ -1,16 +1,24 @@
 package sitemapper
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
-func SiteMap(site string) *http.Response {
+func SiteMap(site string) (*http.Response, error) {
 	resp, err := http.Get(site)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 
-	return resp
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("err: %s ", err)
+		return nil, err
+	}
+	fmt.Println(string(body))
+	return resp, nil
 }
